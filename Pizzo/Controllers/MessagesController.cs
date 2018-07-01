c23f076fe12c25e95b35ef83382d379c5d589a5f
Sumit Das <sumitdas9234@gmail.com>
2018-07-01 13:51:56 +0530

    Resolving Merge Conflict in MessagesController.cs

--- Pizzo/Controllers/MessagesController.cs
+++ Pizzo/Controllers/MessagesController.cs
@@ -41,6 +41,32 @@ namespace Pizzo
                 // Handle conversation state changes, like members being added and removed
                 // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
                 // Not available in all channels
+                //create the static context for card
+                string title = "Welcome to Gusto Pizza";
+                string subtitle = "Address, Gusto Pizza, Bangalore";
+                string text = "Thanks for choosing Gusto Pizza. Help yourself with some tasty pizza!";
+                string imageUrl = "https://image.freepik.com/free-vector/flat-design-pizza-background_23-2147640743.jpg";
+                CardAction button = new CardAction(ActionTypes.OpenUrl, "Locate US", value: "http://restaurants.pizzahut.co.in/");
+
+                IConversationUpdateActivity update = message;
+                var client = new ConnectorClient(new Uri(message.ServiceUrl));
+                if (update.MembersAdded != null && update.MembersAdded.Any())
+                {
+                    foreach (var newMember in update.MembersAdded)
+                    {
+                        if (newMember.Id != message.Recipient.Id)
+                        {
+                            var reply = message.CreateReply();
+                            var attachment = Cards.DynamicCardTemplates.getHeroCard(title, subtitle, text, imageUrl, button);
+                            reply.Attachments.Add(attachment);
+                            reply.Text = "Welcome pizza lover!" +
+                                " In mood for a tasty pizza? Type \"Hi\" to continue";
+
+
+                            client.Conversations.ReplyToActivityAsync(reply);
+                        }
+                    }
+                }
             }
             else if (messageType == ActivityTypes.ContactRelationUpdate)
             {
