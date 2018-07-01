c64dfe4231a10493ce7465c5b014682f39a355cf
Sumit Das <sumitdas9234@gmail.com>
2018-07-01 14:04:50 +0530

    Merge Conflict Resolution

--- Pizzo/Dialogs/RootDialog.cs
+++ Pizzo/Dialogs/RootDialog.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Connector;
@@ -10,31 +12,41 @@ namespace Pizzo.Dialogs
     {
         public Task StartAsync(IDialogContext context)
         {
-            context.Wait(ShowWelcomeBanner);
+            context.Wait(PizzaOptions);
             return Task.CompletedTask;
         }
 
-        private async Task ShowWelcomeBanner(IDialogContext context, IAwaitable<object> result)
+        public async Task PizzaOptions(IDialogContext context, IAwaitable<object> result)
         {
 
-            //create the static context for card
-            string title = "Welcome to Gusto Pizza";
-            string subtitle = "Address, Gusto Pizza, Bangalore";
-            string text = "Thanks for choosing Gusto Pizza. Help yourself with some tasty pizza!";
-            string imageUrl = "https://image.freepik.com/free-vector/flat-design-pizza-background_23-2147640743.jpg";
-            CardAction button = new CardAction(ActionTypes.OpenUrl, "Locate US", value: "http://restaurants.pizzahut.co.in/");
-
-
-            //show the default welcome card
             var message = context.MakeMessage();
-            //call the static getHeroCard() method
-            var attachment = Card.getHeroCard(title, subtitle, text, imageUrl, button);
-            //attach the card
-            message.Attachments.Add(attachment);
-            //wait for the message
+
+            //setting the layout of the attachments to carousel type
+            message.AttachmentLayout = AttachmentLayoutTypes.Carousel;
+
+            //creating a list of message attachments which will be shown in carousel layout
+            message.Attachments = new List<Attachment>();
+
+            //creating hero cards showing pizza options
+            var attachment1 = Cards.DynamicCardTemplates.getHeroCard("Cheese Margherita", "", "", "https://www.google.co.in/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&ved=2ahUKEwiU1sPwovzbAhUKWX0KHTG2DgMQjRx6BAgBEAU&url=https%3A%2F%2Fwww.dominos.co.in%2Fmenu%2Fveg-pizzas%2Fdouble-cheese-margherita&psig=AOvVaw1ajDTwwvfZVlDt_Dy6fP5E&ust=1530478499680860", new CardAction(ActionTypes.ImBack, "Add", value: "Adding Cheese Margherita"));
+            var attachment2 = Cards.DynamicCardTemplates.getHeroCard("Farmhouse", "", "", "https://www.google.co.in/imgres?imgurl=https%3A%2F%2Fwww.dominos.co.in%2F%2Ffiles%2Fitems%2FFarmhouse.jpg&imgrefurl=https%3A%2F%2Fwww.dominos.co.in%2Fmenu%2Fveg-pizzas%2Ffarm-house&docid=ehA3DSH7XRYw8M&tbnid=6jjqSvEv1VMlDM%3A&vet=10ahUKEwiix7TTo_zbAhVGfH0KHV19DecQMwh6KAAwAA..i&w=267&h=265&bih=635&biw=1366&q=farmhouse%20pizza&ved=0ahUKEwiix7TTo_zbAhVGfH0KHV19DecQMwh6KAAwAA&iact=mrc&uact=8", new CardAction(ActionTypes.ImBack, "Add", value: "Adding Farmhouse"));
+
+            //adding the created hero cards to the list of message attachments 
+            message.Attachments.Add(attachment1);
+            message.Attachments.Add(attachment2);
+
+            //posting the hero cards carousel to the bot 
             await context.PostAsync(message);
-            //reply back the Banner
-            context.Wait(ShowWelcomeBanner);
+
+            //function call to handle button click to add a pizza
+            context.Wait(AddPizza);
+
+        }
+
+        public async Task AddPizza(IDialogContext context, IAwaitable<object> result)
+        {
+
         }
     }
 }
+
