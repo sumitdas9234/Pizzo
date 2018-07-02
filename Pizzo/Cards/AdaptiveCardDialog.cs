93a9fe18a7aa119da73b03018e095c92bb1dcd54
Sumit Das <sumitdas9234@gmail.com>
2018-07-02 15:31:40 +0530

    Hero  Cards replaced by Adaptive Cards in Carousel

---
+++ Pizzo/Cards/AdaptiveCardDialog.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AdaptiveCards;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+
+namespace Pizzo.Cards
+{
+    public class AdaptiveCardDialog
+    {
+        public static Attachment CreateAdaptiveCard()
+        {
+            //creating the layout using single column
+            var card = new AdaptiveCard();
+            //adding Image to the body
+                card.Body.Add(new Image()
+                  {
+                      Url = "https://image.freepik.com/free-vector/flat-design-pizza-background_23-2147640743.jpg",
+                      Size = ImageSize.Auto,
+                      Style = ImageStyle.Normal,
+                      AltText = " Pizza Name"
+                  });
+            //adding title to the body
+            card.Body.Add(new TextBlock()
+            {
+                Text = "Pizza Name",
+                Weight = TextWeight.Bolder,
+                Size = TextSize.Medium,
+                Color = TextColor.Dark
+            });
+
+            //Adding the ingredients
+            card.Body.Add(new TextBlock()
+            {
+                Text = "with some awesome flavours and tasty spices sprinkled on the top.",
+                Weight = TextWeight.Normal,
+                Color = TextColor.Accent,
+                Wrap = true
+            });
+
+            //Adding the Price
+            card.Body.Add(new TextBlock()
+            {
+                Text = "Price: $29",
+                Size = TextSize.Small,
+                Weight = TextWeight.Bolder,
+                Color = TextColor.Dark
+            });
+
+            //Adding the Add to cart button
+            card.Actions.Add(new SubmitAction()
+            {
+                Title = "Add to Cart",
+                Data = "Pizza Name"
+            });
+
+            //Converting the adaptive card into an attachment
+            Attachment attachment = new Attachment()
+            {
+                //set the content to AdaptiveCard
+                ContentType = AdaptiveCard.ContentType,
+                // Set the content to card
+                Content = card
+            };
+            return attachment;
+        }
+
+    }
+}
--- Pizzo/Dialogs/RootDialog.cs
+++ Pizzo/Dialogs/RootDialog.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Connector;
+using Pizzo.Cards;
 
 namespace Pizzo.Dialogs
 {
@@ -27,10 +28,11 @@ namespace Pizzo.Dialogs
             message.Attachments = new List<Attachment>();
 
             //creating hero cards showing pizza options
-            var attachment1 = Cards.DynamicCardTemplates.getHeroCard("Cheese Margherita", "", "", "https://www.google.co.in/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&ved=2ahUKEwiU1sPwovzbAhUKWX0KHTG2DgMQjRx6BAgBEAU&url=https%3A%2F%2Fwww.dominos.co.in%2Fmenu%2Fveg-pizzas%2Fdouble-cheese-margherita&psig=AOvVaw1ajDTwwvfZVlDt_Dy6fP5E&ust=1530478499680860", new CardAction(ActionTypes.ImBack, "Add", value: "Adding Cheese Margherita"));
-            var attachment2 = Cards.DynamicCardTemplates.getHeroCard("Farmhouse", "", "", "https://www.google.co.in/imgres?imgurl=https%3A%2F%2Fwww.dominos.co.in%2F%2Ffiles%2Fitems%2FFarmhouse.jpg&imgrefurl=https%3A%2F%2Fwww.dominos.co.in%2Fmenu%2Fveg-pizzas%2Ffarm-house&docid=ehA3DSH7XRYw8M&tbnid=6jjqSvEv1VMlDM%3A&vet=10ahUKEwiix7TTo_zbAhVGfH0KHV19DecQMwh6KAAwAA..i&w=267&h=265&bih=635&biw=1366&q=farmhouse%20pizza&ved=0ahUKEwiix7TTo_zbAhVGfH0KHV19DecQMwh6KAAwAA&iact=mrc&uact=8", new CardAction(ActionTypes.ImBack, "Add", value: "Adding Farmhouse"));
+           
+            var attachment1 = AdaptiveCardDialog.CreateAdaptiveCard();
+            var attachment2 = AdaptiveCardDialog.CreateAdaptiveCard();
 
-            //adding the created hero cards to the list of message attachments 
+            //adding the created Adaptive cards to the list of message attachments 
             message.Attachments.Add(attachment1);
             message.Attachments.Add(attachment2);
             //posting the hero cards carousel to the bot 
