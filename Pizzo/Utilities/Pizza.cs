e618c8bfb229999510c5b92a6105058666b33a71
Sumit Das <sumitdas9234@gmail.com>
2018-07-04 11:31:06 +0530

    Addiion of Helper Classes for the Menu

--- Pizzo/Utilities/Pizza.cs
+++ Pizzo/Utilities/Pizza.cs
@@ -1,33 +1,23 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
 namespace Pizzo.Utilities
 {
-    //public class Veg
-    //{
-    //    public string name { get; set; }
-    //    public string desc { get; set; }
-    //    public int price { get; set; }
-    //    public string image { get; set; }
-    //}
+   
+        public class Pizza
+        {
+            public string name { get; set; }
+            public string desc { get; set; }
+            public int price { get; set; }
+            public string image { get; set; }
+        }
 
-    //public class Nonveg
-    //{
-    //    public string name { get; set; }
-    //    public string desc { get; set; }
-    //    public int price { get; set; }
-    //    public string image { get; set; }
-    //}
-
-    public class Pizza
-    {
-        public string name { get; set; }
-        public string desc { get; set; }
-        public int price { get; set; }
-        public string image { get; set; }
-    }
-
-    public class MenuItem
-    {
-        public List<Pizza> veg { get; set; }
-        public List<Pizza> nonveg { get; set; }
-    }
+        public class MenuItem
+        {
+            public List<Pizza> veg { get; set; }
+            public List<Pizza> nonveg { get; set; }
+        }
+    
 }
